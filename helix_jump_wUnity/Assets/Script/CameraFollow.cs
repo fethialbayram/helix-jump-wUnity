34f83c4d6815cff67d0398d3a85dfcf524dbcf33
fethialbayram <fethi.grlp@gmail.com>
2023-05-10 02:10:28 +0300

    CamerFollow script implemented

--- helix_jump_wUnity/Assets/Script/CameraFollow.cs
+++ helix_jump_wUnity/Assets/Script/CameraFollow.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform target;
+    public Vector3 offset;
+    public float smoothSpeed = 0.04f;
+
+    private void Start()
     {
-        
+        offset = transform.position - target.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        Vector3 newPos = Vector3.Lerp(transform.position, target.position + offset, smoothSpeed);
+        transform.position = newPos;
     }
 }
