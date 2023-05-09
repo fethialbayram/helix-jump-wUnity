f30822a69d67f5457a023567b82686ee92e0e1ab
fethialbayram <fethi.grlp@gmail.com>
2023-05-10 01:09:59 +0300

    HelixManager script created, SpawnRings and Start functions implemented

--- helix_jump_wUnity/Assets/Script/HelixManager.cs
+++ helix_jump_wUnity/Assets/Script/HelixManager.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class HelixManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject[] rings;
+
+    public int noOfRings = 10;
+    public float ringDistance = 5f;
+    float yPos;
+
+    void SpawnRings(int index)
     {
-        
+
+        GameObject newRing = (rings[index],new Vec (transform.position))
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
